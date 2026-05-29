843b7516dec1fe2ca33cf61e904f373cf959d92e
Claus Heussinger <heussinger@xplace.eu>
2026-05-29 12:22:13 +0200

    changed dashboard

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -12,7 +12,7 @@ export default function Dashboard() {
     <>
       <h1><a href="https://vite.dev" target="_blank">
             <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>Async Frontend <a href="https://react.dev" target="_blank">
+        </a>React Frontend <a href="https://react.dev" target="_blank">
             <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </h1>
